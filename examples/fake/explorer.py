f31bdfa7da4b50407d705aa7bbb6fae55a867d9f
Serghei <siracencoserghei@gmail.com>
2024-04-20 23:06:38 +0200

    did the fake and modified endpoinds

--- examples/fake/explorer.py
+++ examples/fake/explorer.py
@@ -1,6 +1,6 @@
 from model.explorer import Explorer
 
-
+# fake data, replaced in Chapter 10 by a real database and SQL
 _explorers = [
     Explorer(
         name="Claude Hande", 
@@ -12,9 +12,11 @@ _explorers = [
         description="Myopic machete man"),
 ]
 
+
 def get_all() -> list[Explorer]:
     """Return all explorers"""
     return _explorers
+
 
 def get_one(name: str) -> Explorer | None:
     """Return one explorer or None"""    
@@ -23,18 +25,25 @@ def get_one(name: str) -> Explorer | None:
             return _explorer
     return None
 
+
 # The following are nonfunctional for now,
 # so they just act like they work, without modifying
 # the actual fake _explorers list:
 def create(explorer: Explorer) -> Explorer:
     """Add an explorer"""
     return explorer
+
+
 def modify(explorer: Explorer) -> Explorer:
     """Partially modify an explorer"""
     return explorer
+
+
 def replace(explorer: Explorer) -> Explorer:
     """Completely replace an explorer"""
     return explorer
+
+
 def delete(name: str) -> bool:
     """Delete an explorer; return None if it existed"""
     return None
--- examples/web/explorer.py
+++ examples/web/explorer.py
@@ -1,8 +1,32 @@
 from fastapi import APIRouter
+from model.explorer import Explorer
+import fake.explorer as service
 
 
 router = APIRouter(prefix="/explorer")
 
 @router.get("/")
-async def top():
-    return "top explorer endpoint"
+async def get_all():
+    return service.get_all()
+
+@router.get("/{name}")
+async def get_one(name: str)-> Explorer | None:
+    return service.get_one(name)
+
+# all the remaining endpoints do nothing yet:
+@router.post("/")
+async def create(explorer: Explorer)-> Explorer:
+    return service.create(explorer)
+
+@router.patch("/")
+async def modify(explorer: Explorer)-> Explorer:
+    return service.modify(explorer)
+
+@ router.put("/")
+async def replace(explorer: Explorer)-> Explorer:
+    return service.replace(explorer)
+
+@router.delete("/{name}")
+async def delete(name: str)-> None:
+    return None
+
